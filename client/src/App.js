909fcc6a04a49f7207e8b0a64ba4cb8690c8c5fa
jjobe5989 <s3thjobe@gmail.com>
2023-03-19 03:59:30 -0500

    fixed most issues but cannot create an account

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost'
 import SearchBooks from './pages/SearchBooks';
@@ -26,11 +26,11 @@ function App() {
        <Router>
       <>
         <Navbar />
-        <Switch>
+        <Routes>
           <Route exact path='/' component={SearchBooks} />
           <Route exact path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
+        </Routes>
       </>
     </Router>
   </ApolloProvider>
